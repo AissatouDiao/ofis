85a0381e98cc0266da182039d2d7250797dedfb4
Satou Diao <satoudiao@gmail.com>
2019-12-05 16:19:56 +0000

    commit 8

--- resources/views/dons.blade.php
+++ resources/views/dons.blade.php
@@ -11,8 +11,8 @@ include('../resources/views/header.php');
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 <!-- Fav Icons -->
-<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-<link rel="icon" href="images/favicon.png" type="image/x-icon">
+<link rel="shortcut icon" href="/images/petit-logo.png" type="image/x-icon">
+<link rel="icon" href="/images/petit-logo.png" type="image/x-icon">
 <!-- Responsive -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
@@ -27,13 +27,197 @@ include('../resources/views/header.php');
 		<![endif]-->
 </head>
 <body>
+<div class="page-wrapper">
+    
+    <!-- Preloader -->
+    <div class="preloader"></div> 
+    <!-- main header -->
+   
+<header class="main-header">
+
+<!-- header top -->
+<!-- header top -->
+<div class="header-top dark">
+    <div class="container-fluid">
+        <div class="outer-box">
+            <!--Top Left-->
+            <div class="top-left">
+            @if(Auth::check())
+                @if(Auth::user()->is_admin==0)
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <h5 style="color:white"> <div class="icon"><span class="flaticon-social"> {{ Auth::user()->name }}  {{ Auth::user()->prenom }}</span></div></h5> <span class="caret"></span>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="./myp">Mon Projet</a>
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();" >
+                                            {{ __('Deconnexion') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;background-color:black;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                @elseif(Auth::user()->is_admin==1)
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <h5 style="color:white"> <div class="icon"><span class="flaticon-social"> {{ Auth::user()->name }}</span></div></h5> <span class="caret"></span>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="admin">Administration</a>
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();" >
+                                            {{ __('Deconnexion') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;background-color:black;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                @endif
+            @else
+
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+              <h6 style="color:white"> <div class="icon"><span class="flaticon-social"> Se connecter | S'inscrire</span></div></h6> <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Se connecter') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('S\'inscrire') }}</a>
+                                </li>
+                            @endif
+                                </div>
+            @endif
+            </div>
+
+            <!--Top Right-->
+            <div class="top-right">
+                
+            <div class="social-links clearfix">
+                    <a href="https://www.twitter.com/ofisdusenegal"><span class="fab fa-twitter"></span></a>
+                    <a href="https://www.facebook.com/ofisdusenegal"><span class="fab fa-facebook"></span></a>
+                    <a href="https://www.instagram.com/ofisdusenegal"><span class="fab fa-instagram"></span></a>
+                    <a href="https://www.linkedin.com/ofisdusenegal"><span class="fab fa-linkedin-in"></span></a>
+                </div>
+                
+            </div>
+        </div>
+            
+    </div>
+</div>
+
+<!--Header-Upper-->
+<div class="header-upper ">
+    <div class="container-fluid clearfix">
+            
+    <div class="logo float-left">
+                <a href="./home" class="img-responsive"><img src="/images/grand-logo.png" alt="" title=""></a>
+            </div>
+        <div class="float-right upper-right clearfix">
+            
+            <div class="nav-outer clearfix">
+                <!-- Main Menu -->
+                <nav class="main-menu navbar-expand-lg">
+                    <div class="navbar-header">
+                        <!-- Toggle Button -->      
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    
+                    <div class="navbar-collapse collapse clearfix">
+                        <ul class="navigation clearfix">
+                       
+                        <li class="current" ><a href="{{ url('home') }}">Accueil</a></li>
+                            <li><a href="{{ url('projets' )}}">Nos projets</a></li>
+                            <li><a href="{{ url('blog') }}">Blog</a></li>
+                            <li><a href="{{ url('aboutus') }}">A propos de nous</a></li>
+                            <li><a href="{{ url('contact') }}">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+                
+                <!-- Main Menu End-->
+                
+                <!-- Search box -->
+                <div class="outer-box">
+                    <!--Search outer-->
+                    <div class="search-box-outer">
+                        <div class="dropdown">
+                            <button class="search-box-btn dropdown-toggle" type="button" id="dropdownMenu3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="fa fa-search"></span></button>
+                            <ul class="dropdown-menu pull-right search-panel" aria-labelledby="dropdownMenu3">
+                                <li class="panel-outer">
+                                    <div class="form-container">
+                                        <form method="post" action="blog.html">
+                                            <div class="form-group">
+                                                <input type="search" name="field-name" value="" placeholder="Search Here" required>
+                                                <button type="submit" class="search-btn"><span class="fa fa-search"></span></button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="link-btn"><a href="{{url('donspage')}}" class="theme-btn btn-style-two donate-box-btn">Faire un don</a></div>
+                </div>
+                
+            </div>
+            
+        </div>
+            
+    </div>
+</div>
+<!--End Header Upper-->
+
+<!--Sticky Header-->
+<div class="sticky-header">
+    <div class="container">
+        <div class="clearfix">
+            <!--Logo-->
+            <div class="logo float-left">
+                <a href="./home" class="img-responsive"><img src="/images/petit-logo.png" alt="" title=""></a>
+            </div>
+            
+            <!--Right Col-->
+            <div class="right-col float-right">
+                <!-- Main Menu -->
+                <nav class="main-menu navbar-expand-lg">
+                    <div class="navbar-collapse collapse clearfix">
+                        <ul class="navigation clearfix">
+                            
+                            <li class="current" ><a href="{{ url('home' )}}">Accueil</a></li>
+                            <li><a href="{{ url('projets' )}}">Nos projets</a></li>
+                            <li><a href="{{ url('blog') }}">Blog</a></li>
+                            <li><a href="{{ url('aboutus') }}">A propos de nous</a></li>
+                            <li><a href="{{ url('contact') }}">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav><!-- Main Menu End-->
+            </div>
+        </div>
+            
+        
+    </div>
+</div>
+<!--End Sticky Header-->
+</header>
 <div class="container">
         <div class="donate-form-area">
             <div class="section-title center">
                 <h2>Donation Information</h2>
             </div>
 
-            <h4>How much would you like to donate:</h4>
+            <h4>Faire une proposition de don</h4>
             {!! Form::open(['url' => './dons']) !!}
            
             @csrf
@@ -70,17 +254,24 @@ include('../resources/views/header.php');
                     </li>
                 </ul>
 
-                <h3>Informations du donneur</h3>
+                <h3>Informations sur le don</h3>
 
                 <div class="form-bg">
                     <div class="row clearfix">
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                            
                             <div class="form-group {!! $errors->has('nom') ? 'has-error' : '' !!}">
-                                <p>Your Name*</p>
+                                <p>Nom</p>
                                 
                                 <input type="text" name="nom" placeholder="">
                                 {!! $errors->first('nom', '<small class="help-block">:message</small>') !!}
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="form-group {!! $errors->has('prenom') ? 'has-error' : '' !!}">
+                                <p>Prénom</p>
+                                
+                                <input type="text" name="prenom" placeholder="">
+                                {!! $errors->first('prenom', '<small class="help-block">:message</small>') !!}
                             </div>
                         </div>
                         <div class="col-md-6 col-sm-6 col-xs-12">
@@ -110,41 +301,17 @@ include('../resources/views/header.php');
 
                 <ul class="payment-option">
                     <li>
-                        <h4>Choisir son mode de payment</h4>
+                        <h4>Choisir de rester anonyme</h4>
                     </li>
                     <li>
                         <div class="checkbox">
                             <label>
-                                <input name="pay-us" type="checkbox">
-                                <span>Paypal</span>
+                                <input name="anonyme" type="checkbox">
+                                <span>Anonymat</span>
                             </label>
                         </div>
                     </li>
-                    <li>
-                       <div class="checkbox">
-                            <label>
-                                <input name="pay-us" type="checkbox">
-                                <span>Offline Donation</span>
-                            </label>
-                        </div> 
-                    </li>
-                    <li>
-                        <div class="checkbox">
-                            <label>
-                                <input name="pay-us" type="checkbox">
-                                <span>Credit Card</span>
-                            </label>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="checkbox">
-                            <label>
-                                <input name="pay-us" type="checkbox">
-                                <span>Debit Card</span>
-                            </label>
-                        </div>
-                    </li>
-                </ul>
+                   
 
                 <div class="center"><button class="theme-btn btn-style-one" type="submit" name="ok">Faire un don</button></div>
                     
