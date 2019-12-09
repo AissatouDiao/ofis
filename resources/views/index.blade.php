2721d9e99e5f2b235126220b0a0823983293aa12
Satou Diao <satoudiao@gmail.com>
2019-12-09 23:34:20 +0000

    commit 38

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -361,7 +361,7 @@ include('../resources/views/header.php');
         <div class="container">
             <!--Sec Title-->
             <div class="sec-title centered">
-                <div class="icon-box"><img src="/images/fusee.png" alt="" ></div>
+                <div class="icon-box"><img src="../resources/images/fusee.png" alt="" ></div>
                 <h1>Projets <span>en cours</span></h1>
                 <div class="text"></div>
             </div>
@@ -369,53 +369,56 @@ include('../resources/views/header.php');
                
                 @foreach($projets as $projet)
                
-                    <!--Cause Block-->
-                    <div class="cause-block col-lg-4 col-md-6 ">
-                        <div class="inner-box">
-                            <figure class="image">
-                                <img src="./{{$projet->image}}" style="width:360px;height:280px" alt="">
-                                <div class="overlay">
-                                </div>
-                            </figure>
-                            <div class="lower-content">
-                                <h5>Toute aide est la bienvenue</h5>
-                                <a href="{{route('lireleprojet', [$projet->id])}}" ><h4>{{$projet->titre}}</h4> </a>  
-                                <div class="text">{!!Str::words($projet->description, 50)!!}...</div><br>
-                                <!--Progress Levels-->
-                                <div class="progress-levels" > 
-                                            <?php
-                                            $MT=$projet->montant_goal;
-                                            $MR=$projet->donsprojets->sum('montant');
-                                            $pourcentage=($MR*100)/$MT;
-                                            ?>
-                                    <!--Skill Box-->
-                                    <div class="progress-box wow fadeInRight" data-wow-delay="100ms" data-wow-duration="1500ms" >
-                                        <div class="inner">
-                                            <div class="bar" >
-                                                <div class="bar-innner"><div class="bar-fill" style="padding:5px;" data-percent="{{round($pourcentage)}}"><div class="percent"></div></div></div>
-                                            </div>
+                <!--Cause Block-->
+                <div class="cause-block col-lg-4 col-md-6 ">
+                    <div class="inner-box">
+                          <figure class="image">
+                            <img src="./{{$projet->image}}" style="width:360px;height:280px" alt="">
+                            <div class="overlay">
+                            </div>
+                           </figure>
+                        <div class="lower-content">
+                            <h5>Toute aide est la bienvenue</h5>
+                            <a href="{{route('lireleprojet', [$projet->id])}}" ><h4>{{$projet->titre}}</h4> </a>  
+                            <div class="text">{!!Str::words($projet->description, 70)!!}...</div><br>
+
+
+                            <!--Progress Levels-->
+                            <div class="progress-levels" > 
+                                        <?php
+                                        $MT=$projet->montant_goal;
+                                        $MR=$projet->donsprojets->sum('montant');
+                                        $pourcentage=($MR*100)/$MT;
+                                        ?>
+                                <!--Skill Box-->
+                                <div class="progress-box wow fadeInRight" data-wow-delay="100ms" data-wow-duration="1500ms" >
+                                    <div class="inner">
+                                        <div class="bar" >
+                                            <div class="bar-innner"><div class="bar-fill" style="padding:5px;" data-percent="{{round($pourcentage)}}"><div class="percent"></div></div></div>
                                         </div>
                                     </div>
+                                </div>
+                                
+                            </div>
+
+                            <div class="donate clearfix">
+                            
+                                <div class="float-left">
+                                    <span>{{$projet->donsprojets->sum('montant')}} 
+                                    </span>
                                     
                                 </div>
-                                <div class="donate clearfix">
-                                
-                                    <div class="float-left">
-                                        <span>{{$projet->donsprojets->sum('montant')}} 
-                                        </span>
-                                        
-                                    </div>
-                                
-                                    <div class="float-right">
-                                        {{$projet->montant_goal}} FCFA
-                                    </div>
-                                </div>  
-                                <div class="link-btn"><a href="./donsurprojet/{{$projet->id}}" class="theme-btn btn-style-four donate-box-btn">Faire un don</a></div>
-                            </div>
+                             
+                                <div class="float-right">
+                                    {{$projet->montant_goal}} FCFA
+                                </div>
+                            </div>  
+                            
+                            <div class="link-btn"><a href="./donsurprojet/{{$projet->id}}" class="theme-btn btn-style-four donate-box-btn">Faire un don</a></div>
                         </div>
                     </div>
-                    </div>
-                    </div>
+                </div>
+                </div>
                 @endforeach
               
             </div>
@@ -734,37 +737,7 @@ include('../resources/views/header.php');
         </div>
     </section>
 
-    <!-- Testimonial Section 
-    <section class="testimonial-section sp-one">
-        <div class="container">
-
-            <div class="single-item-carousel owl-carousel owl-theme owl-nav-none owl-dot-none">
-                
-                <div class="testimonial-block-one">
-                    <div class="inner-box">
-                        <div class="image">
-                            <img src="images/resource/author.jpg" alt="">
-                        </div>
-                        <div class="text">Since vindictively over agile the some far well besides constructively well airy then one during with close excellent grabbed gosh contrary far dalmatian upheld intrepid bought and toucan majestic more some apart dear boa much cast falcon a dwelled ouch busy. </div>
-                        <h3>Sande Well  <span>ChariteMax Donator</span></h3>
-                    </div>
-                </div>
-
-                <div class="testimonial-block-one">
-                    <div class="inner-box">
-                        <div class="image">
-                            <img src="images/resource/author.jpg" alt="">
-                        </div>
-                        <div class="text">Since vindictively over agile the some far well besides constructively well airy then one during with close excellent grabbed gosh contrary far dalmatian upheld intrepid bought and toucan majestic more some apart dear boa much cast falcon a dwelled ouch busy. </div>
-                        <h3>Sande Well  <span>ChariteMax Donator</span></h3>
-                    </div>
-                </div>
-            
-            </div>
-            
-        </div>
-    </section>-->
-    <!--End Testimonial Section-->
+   
 
     <!-- News Section -->
     <section class="news-section sp-two">
