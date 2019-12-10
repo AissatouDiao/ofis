57ca174d7ab585cfc03cbc7774306c662b4dbb68
Satou Diao <satoudiao@gmail.com>
2019-12-10 16:31:31 +0000

    commit 53

--- app/Http/Requests/DonsRequest.php
+++ app/Http/Requests/DonsRequest.php
@@ -29,7 +29,7 @@ class DonsRequest extends FormRequest
             'adresse' => 'required|max:250',
             'telephone'=>'required|numeric|regex:/[0-9]{7,15}/',    
             'montant'=>'required|integer',
-            'is_ano'=>'bolean',
+            'is_ano'=>'accepted',
         ];
     }
 }
