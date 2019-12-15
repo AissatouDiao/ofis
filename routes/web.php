ff3d70205c86c9c03a74b060955c3927dbcb6182
Satou Diao <satoudiao@gmail.com>
2019-12-15 08:13:48 +0000

    commit 92

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ Route::delete('/supprimer/{id}', 'CommentController@delete')->name('supprimer');
 Route::post('/reponse/{comments}', 'ReplyController@reply')->name('reponse');
 
 Route::post('/commentaire1/{projet}', 'CommentaireprojetController@commentaire')->name('projetcommentaire');
-Route::post('/reponse1/{commentaire}', 'ReponsecommentaireprojetController@reponse')->name('reponsecommentaire');
+Route::post('/reponseP/{commentaire}', 'ReponsecommentaireprojetController@reponse')->name('reponsecommentaire');
 
 
 
