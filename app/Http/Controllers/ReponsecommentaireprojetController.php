a809e18c468ae8185bbc90418f733f5cc272d3f6
Satou Diao <satoudiao@gmail.com>
2019-12-15 06:22:20 +0000

    commit 89

--- app/Http/Controllers/ReponsecommentaireprojetController.php
+++ app/Http/Controllers/ReponsecommentaireprojetController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\RcRequest;
+use Illuminate\Http\Requests\RcRequest;
 use App\Reponseprojetcommentaire;
 use App\Projetcommentaire;
 
