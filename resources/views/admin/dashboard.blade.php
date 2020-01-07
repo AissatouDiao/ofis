fdf219acd4d597b7b2219ac2cc11bd43523104a3
Satou Diao <satoudiao@gmail.com>
2020-01-07 16:45:01 +0000

    commit 127

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -364,10 +364,10 @@ include('../resources/views/admin/nav.php');
 
       <div style = "width: 80%; margin: 0 auto;">
           <div style = " width : 80 %; margin : 0 auto ; " >    
-            {!! $chart-> container () !!}
-        </div>
+            {!! $chart->container () !!}
+        </div></div>
         <script src = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset = "utf-8"> </script>
-        {!! $chart-> script () !!}
+        {!! $chart->script () !!}
       <footer class="footer">
         <div class="container-fluid">
           <nav>
