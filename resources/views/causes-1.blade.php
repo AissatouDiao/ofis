3ed90fd1fcc038d571b7c51540ab7e23059da193
Satou Diao <satoudiao@gmail.com>
2020-01-09 12:01:25 +0000

    commit 142

--- resources/views/causes-1.blade.php
+++ resources/views/causes-1.blade.php
@@ -236,7 +236,7 @@
                             <div class="lower-content">
                                 <h5></h5>
                                 <h4><a href="{{route('lireleprojet', [$projet->id])}}">{{$projet->titre}}</a></h4>
-                                <div class="text">{!!Str::words($projet->description,30)!!}...</div>
+                                <div class="text">{!!Str::words($projet->description, 50)!!}...</div>
                                 <?php $MT=$projet->montant_goal;$MR=$projet->donsprojets->sum('montant');$pourcentage=($MR*100)/$MT;?>
                                 <!--Progress Levels-->
                                 <div class="progress-levels">
